231bfd18c37f587b8589df73f57da00e22861bbb
Martin Hartmann <martinarielhartmann@gmail.com>
2025-12-16 17:22:15 +0200

    mcanimate: ax and el options

--- src/mcanimate.jl
+++ src/mcanimate.jl
@@ -1,4 +1,5 @@
-function mcanimate(m::mocapdata; framerate = m.freq, filename = "animation.mp4")
+using Infiltrator
+function mcanimate(m::mocapdata; framerate = m.freq, filename = "animation.mp4",azimuth=0,elevation=0)
     data = Matrix(m.data)
     X = data[:, 1:3:end]
     Y = data[:, 2:3:end]
@@ -8,13 +9,14 @@ function mcanimate(m::mocapdata; framerate = m.freq, filename = "animation.mp4")
     ylim=extrema(filter(!isnan,Y))
     zlim=extrema(filter(!isnan,Z))
     ax = Axis3(fig[1, 1],aspect=:data,limits = (xlim[1], xlim[2], ylim[1], ylim[2], zlim[1], zlim[2]))
-    ax.azimuth = 0
-    ax.elevation = 0
+    ax.azimuth = azimuth
+    ax.elevation = elevation
     hidespines!(ax)
     hidedecorations!(ax)
 
     p = meshscatter!(ax,X[1,:],Y[1,:],Z[1,:],markersize=20)
 
+    @infiltrate
     conn = [(p[1][][i-1], p[1][][i]) for i in 2:length(p[1][])]
     pl = linesegments!(ax, conn,color=:blue)
 
