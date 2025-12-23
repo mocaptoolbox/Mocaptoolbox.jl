f44876fc6a8ad5e79c73ad9fd7807242d8a286a3
Martin Hartmann <martinarielhartmann@gmail.com>
2025-12-23 11:09:52 +0200

    mcmerge: add support for marker connections

--- src/mcmerge.jl
+++ src/mcmerge.jl
@@ -7,9 +7,18 @@ function mcmerge(m::Mocapdata...)
         truncate(dfs) = [df[1:minlen,:] for df in dfs]
         dfs = truncate(dfs)
     end
+    for k = 2:length(m)
+        if sum(m[k].conn) > 0
+            m[k].conn .+= maximum(m[k-1].conn)
+        end
+    end
     dfs = hcat(dfs...,makeunique=true)
     res = deepcopy(m[1])
+
     res.data = dfs
+    res.conn = vcat([x.conn for x in m]...)
+    res.markerName = vcat([x.markerName for x in m]...)
+    res.nMarkers = sum([x.nMarkers for x in m])
     return res
 end
 
