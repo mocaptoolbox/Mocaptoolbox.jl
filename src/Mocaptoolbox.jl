f838c9507f0c5d74b1ffcaafb7dd005cb726acc8
Martin Hartmann <martinarielhartmann@gmail.com>
2025-12-23 11:08:07 +0200

    add new functions, dependencies

--- src/Mocaptoolbox.jl
+++ src/Mocaptoolbox.jl
@@ -1,11 +1,14 @@
 module Mocaptoolbox
 
-using CSV, DataFrames, GLMakie, SavitzkyGolay
+using CSV, DataFrames, GLMakie, SavitzkyGolay, DataInterpolations, Statistics, LinearAlgebra
 
 import Base: *
 
 export mcread
 include("mcread.jl")
+
+export mcplotframe
+include("mcplotframe.jl")
 
 export mcanimate
 include("mcanimate.jl")
@@ -15,5 +18,35 @@ include("mcmerge.jl")
 
 export mctimeder
 include("mctimeder.jl")
+
+export mcresample
+include("mcresample.jl")
+
+export mctrim
+include("mctrim.jl")
+
+export mcgetmarker
+include("mcgetmarker.jl")
+
+export mcnorm
+include("mcnorm.jl")
+
+export mccorr
+include("mccorr.jl")
+
+export mccomplexity
+include("mccomplexity.jl")
+
+export mcfillgaps
+include("mcfillgaps.jl")
+
+export mcfluidity
+include("mcfluidity.jl")
+
+export plot
+include("plot.jl")
+
+export heatmap
+include("heatmap.jl")
 
 end
