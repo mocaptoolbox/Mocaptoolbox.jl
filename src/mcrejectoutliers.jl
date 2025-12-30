f185ccc50b8cc2b013c398c0a6b95cd4bc2f4467
Martin Hartmann <martinarielhartmann@gmail.com>
2025-12-30 22:12:32 +0200

    drop nanmedian argument for speed

--- src/mcrejectoutliers.jl
+++ src/mcrejectoutliers.jl
@@ -3,23 +3,24 @@
     Example: If the distance between two markers for a given time point is higher or lower than `thres`, both markers are converted to NaNs for that time point.
     Thres is in units of measurement, e.g. default is 250 mm for Qualisys MoCap/Theia3D data.
     """
-function mcrejectoutliers(m::Mocapdata;fun=nanmedian::Function,thres=250::Real)
+function mcrejectoutliers(m::Mocapdata;thres=250::Real)
     x = Matrix(m.data)
     r,c = size(x)
-    dm = Array{Float64}(undef,m.nMarkers,m.nMarkers,r);
-    rs = [Matrix{Float64}(undef,3,m.nMarkers) for _ in 1:r];
+    nMarkers = m.nMarkers
+    dm = Array{Float64}(undef,nMarkers,nMarkers,r);
+    rs = [Matrix{Float64}(undef,3,nMarkers) for _ in 1:r];
     for k = 1:r
         rs[k] = reshape(x[k,:],3,:);
         dm[:,:,k] = pairwise(Euclidean(), rs[k], dims=2);
     end
-    applyfun(dm,fun) = dropdims(fun(dm,dims=3),dims=3)
-    absdev = abs.(dm .- applyfun(dm,fun))
+    med(dm) = dropdims(nanmedian(dm,dims=3),dims=3)
+    absdev = abs.(dm .- med(dm))
     absdev[absdev .> thres] .= NaN
     for f = 1:r
-        for k = 1:m.nMarkers-1
-            for j = (k+1):m.nMarkers
+        for k = 1:nMarkers-1
+            for j = (k+1):nMarkers
                 if isnan(absdev[k,j,f])
-                    rs[f][:,[k,j]] .= NaN
+                  @inbounds rs[f][:,[k,j]] .= NaN
                 end
             end
         end
