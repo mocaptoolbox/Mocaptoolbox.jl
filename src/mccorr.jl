23b8308cc4db73a05b966d8e7b78d1bb801be131
Martin Hartmann <martinarielhartmann@gmail.com>
2026-01-01 10:31:22 +0200

    optimization

--- src/mccorr.jl
+++ src/mccorr.jl
@@ -1,8 +1,10 @@
 function mccorr(m1::Union{Mocapdata,Normdata},m2::Union{Mocapdata,Normdata})
-    c = diag(cor(Matrix(m1.data),Matrix(m2.data)))
+    n = size(m1.data, 2)
+    @views c = [cor(m1.data[:, i], m2.data[:, i]) for i in 1:n]
     return DataFrame(c',names(m1.data))
 end
-function mccorr(m1::Mocapdata,m2::Mocapdata,dim)
-    c = diag(cor(Matrix(m1.data[:,dim:3:end]),Matrix(m2.data[:,dim:3:end])),0)
-    return DataFrame(c',m1.markerName)
+function mccorr(m1::Mocapdata, m2::Mocapdata, dim)
+    n = m1.nMarkers
+    @views c = [cor(m1.data[:, dim + 3*(i-1)], m2.data[:, dim + 3*(i-1)]) for i in 1:n]
+    return DataFrame(c', m1.markerName)
 end
