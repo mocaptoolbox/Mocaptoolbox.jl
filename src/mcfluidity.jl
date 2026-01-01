eba6157c0c0e1341bb8b18061cb9c88d6d75c032
Martin Hartmann <martinarielhartmann@gmail.com>
2026-01-01 10:30:52 +0200

    add expected types

--- src/mcfluidity.jl
+++ src/mcfluidity.jl
@@ -1,7 +1,7 @@
 function mcfluidity(m::Mocapdata)
     v = mctimeder(m)
     a = mctimeder(v)
-    vm = nanmean(Matrix(mcnorm(v).data),dims=1)
-    am = nanmean(Matrix(mcnorm(a).data),dims=1)
+    vm = nanmean(Matrix(mcnorm(v).data),dims=1)::Matrix{Float64}
+    am = nanmean(Matrix(mcnorm(a).data),dims=1)::Matrix{Float64}
     return (vm/am)[1];
 end
