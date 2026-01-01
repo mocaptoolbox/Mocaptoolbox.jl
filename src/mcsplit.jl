3e6529e814c87a6b41d060d3f02d926300617f25
Martin Hartmann <martinarielhartmann@gmail.com>
2026-01-01 10:25:49 +0200

    fix bug in nMarkers, optimize

--- src/mcsplit.jl
+++ src/mcsplit.jl
@@ -10,19 +10,17 @@ function mcsplit(m::Mocapdata,parts=2::Int)
     ndimspart = div(c,parts)
     nmarkerspart = div(m.nMarkers,parts)
     nconnpart = div(r2,parts)
-    v = [zeros(r, ndimspart) for i in 1:parts]
     d = Vector{Mocapdata}(undef,parts)
     winfun(x,k) = collect((1:x).+x*(k-1))
     for k = 1:parts
         dims = winfun(ndimspart,k)
         dimsmarker = winfun(nmarkerspart,k)
         dimsconn = winfun(nconnpart,k)
-        v[k] = Matrix(m.data[:,dims])
         d[k] = deepcopy(m)
-        d[k].data = DataFrame(v[k],names(m.data)[dims])
+        d[k].data = DataFrame(Matrix(m.data[:,dims]),names(m.data)[dims])
         d[k].conn = m.conn[dimsconn,:]
         d[k].markerName = m.markerName[dimsmarker]
-        d[k].nMarkers = div(m.nMarkers,nmarkerspart)
+        d[k].nMarkers = nmarkerspart
     end
     return d
 end
@@ -32,14 +30,12 @@ function mcsplit(m::Normdata,parts=2::Int)
         error("Cannot split data into even number of parts")
     end
     ndimspart = div(c,parts)
-    v = [zeros(r, ndimspart) for i in 1:parts]
     d = Vector{Normdata}(undef,parts)
     winfun(x,k) = collect((1:x).+x*(k-1))
     for k = 1:parts
         dims = winfun(ndimspart,k)
-        v[k] = Matrix(m.data[:,dims])
         d[k] = deepcopy(m)
-        d[k].data = DataFrame(v[k],names(m.data)[dims])
+        d[k].data = DataFrame(Matrix(m.data[:,dims]),names(m.data)[dims])
         d[k].markerName = m.markerName[dims]
         d[k].nMarkers = div(m.nMarkers,ndimspart)
     end
