881e874ea62860e68b873166d055502cf6f35e48
Martin Hartmann <martinarielhartmann@gmail.com>
2026-01-01 10:30:41 +0200

    optimization

--- src/mcgetmarker.jl
+++ src/mcgetmarker.jl
@@ -1,45 +1,28 @@
 function mcgetmarker(m::Mocapdata,mnum::Int)
-    SelectMarkerDF(m,mnum) = m.data[:,getMarkerInd(mnum)]
     g = deepcopy(m)
     g.data = SelectMarkerDF(m,mnum)
-    getMarkerNameFromMarkerNum(m,mnum) = [m.markerName[mnum]]
+    g.markerName = [getMarkerNameFromMarkerNum(m,mnum)]
+    g.nMarkers = length(mnum)
+    return g
+end
+function mcgetmarker(m::Union{Mocapdata,Normdata}, mnum::Union{Vector{Int64},UnitRange{Int64}})
+    g = deepcopy(m)
+    g.data = SelectMarkerDF(m,mnum)
     g.markerName = getMarkerNameFromMarkerNum(m,mnum)
     g.nMarkers = length(mnum)
     return g
 end
-function mcgetmarker(m::Mocapdata, mnum::Vector{Int64})
-    SelectMarkerDF(m,mnum) = m.data[:,vcat(getMarkerInd.(vec(mnum))...)]
-    g = deepcopy(m)
-    g.data = SelectMarkerDF(m,mnum)
-    getMarkerNameFromMarkerNum(m,mnum) = m.markerName[mnum]
-    g.markerName = getMarkerNameFromMarkerNum(m,mnum)
-    g.nMarkers = length(mnum)
-    return g
-end
-function mcgetmarker(m::Mocapdata, mnum::UnitRange{Int64})
-    SelectMarkerDF(m,mnum) = m.data[:,vcat(getMarkerInd.(collect(mnum))...)]
-    g = deepcopy(m)
-    g.data = SelectMarkerDF(m,mnum)
-    getMarkerNameFromMarkerNum(m,mnum) = m.markerName[mnum]
-    g.markerName = getMarkerNameFromMarkerNum(m,mnum)
-    g.nMarkers = length(mnum)
-    return g
-end
-function mcgetmarker(m::Mocapdata,mname::String)
-    getMarkerNameInd(m,mname) = findall(m.markerName .== mname)
-    SelectMarkerDF(m,mname) = m.data[:,getMarkerInd(getMarkerNameInd(m,mname))]
+function mcgetmarker(m::Union{Mocapdata,Normdata},mname::String)
     g = deepcopy(m)
     g.data = SelectMarkerDF(m,mname)
     g.markerName = [mname]
     g.nMarkers = length([mname])
     return g
 end
-function mcgetmarker(m::Mocapdata,mname::Vector{String})
+function mcgetmarker(m::Union{Mocapdata,Normdata},mname::Vector{String})
     if length(unique(m.markerName)) != length(m.markerName)
-        error("Marker names in mocap struct are not unique")
+        error("Marker names are not unique")
     end
-    getMarkerNameInd(m,mname) = indexin(mname,m.markerName)
-    SelectMarkerDF(m,mname) = m.data[:,vcat(getMarkerInd.(getMarkerNameInd(m,mname))...)]
     g = deepcopy(m)
     g.data = SelectMarkerDF(m,mname)
     g.markerName = mname
@@ -49,48 +32,19 @@ end
 function mcgetmarker(m::Normdata,mnum::Int)
     g = deepcopy(m)
     g.data = m.data[:,mnum]
-    getMarkerNameFromMarkerNum(m,mnum) = [m.markerName[mnum]]
-    g.markerName = getMarkerNameFromMarkerNum(m,mnum)
+    g.markerName = [getMarkerNameFromMarkerNum(m,mnum)]
     g.nMarkers = length(mnum)
     return g
 end
-function mcgetmarker(m::Normdata, mnum::Vector{Int64})
-    SelectMarkerDF(m,mnum) = m.data[:,vcat(mnum...)]
-    g = deepcopy(m)
-    g.data = SelectMarkerDF(m,mnum)
-    getMarkerNameFromMarkerNum(m,mnum) = m.markerName[mnum]
-    g.markerName = getMarkerNameFromMarkerNum(m,mnum)
-    g.nMarkers = length(mnum)
-    return g
-end
-function mcgetmarker(m::Normdata, mnum::UnitRange{Int64})
-    SelectMarkerDF(m,mnum) = m.data[:,collect(mnum)]
-    g = deepcopy(m)
-    g.data = SelectMarkerDF(m,mnum)
-    getMarkerNameFromMarkerNum(m,mnum) = m.markerName[mnum]
-    g.markerName = getMarkerNameFromMarkerNum(m,mnum)
-    g.nMarkers = length(mnum)
-    return g
-end
-function mcgetmarker(m::Normdata,mname::String)
-    getMarkerNameInd(m,mname) = findall(m.markerName .== mname)
-    SelectMarkerDF(m,mname) = m.data[:,getMarkerNameInd(m,mname)]
-    g = deepcopy(m)
-    g.data = SelectMarkerDF(m,mname)
-    g.markerName = [mname]
-    g.nMarkers = length([mname])
-    return g
-end
-function mcgetmarker(m::Normdata,mname::Vector{String})
-    if length(unique(m.markerName)) != length(m.markerName)
-        error("Marker names are not unique")
-    end
-    getMarkerNameInd(m,mname) = indexin(mname,m.markerName)
-    SelectMarkerDF(m,mname) = m.data[:,getMarkerNameInd(m,mname)]
-    g = deepcopy(m)
-    g.data = SelectMarkerDF(m,mname)
-    g.markerName = mname
-    g.nMarkers = length(mname)
-    return g
-end
-getMarkerInd(markernameind) = 3*(markernameind.-1) .+ collect(1:3)
+getMarkerInd(i) = (3i-2):(3i)
+getMarkerNameInd(m,mname::String) = findfirst(==(mname), m.markerName)
+getMarkerNameInd(m,mname::Vector{String}) = indexin(mname,m.markerName)
+SelectMarkerDF(m::Normdata,mname) = m.data[:,getMarkerNameInd(m,mname)]
+getMarkerNameFromMarkerNum(m,mnum) = m.markerName[mnum]
+SelectMarkerDF(m::Mocapdata,mnum::Int) = m.data[:,getMarkerInd(mnum)]
+SelectMarkerDF(m::Mocapdata, mnum::Vector{Int64}) = m.data[:,vcat(getMarkerInd.(vec(mnum))...)]
+SelectMarkerDF(m::Mocapdata, mnum::UnitRange{Int64}) = m.data[:,vcat(getMarkerInd.(collect(mnum))...)]
+SelectMarkerDF(m::Mocapdata,mname::String) = m.data[:,getMarkerInd(getMarkerNameInd(m,mname))]
+SelectMarkerDF(m::Mocapdata,mname::Vector{String}) = m.data[:,vcat(getMarkerInd.(getMarkerNameInd(m,mname))...)]
+SelectMarkerDF(m::Normdata, mnum::Vector{Int64}) = m.data[:,vcat(mnum...)]
+SelectMarkerDF(m::Normdata, mnum::UnitRange{Int64}) = m.data[:,collect(mnum)]
