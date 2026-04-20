cc796c1a8b8e5e36924cbec13d180d0f81b2d9fa
Martin Hartmann <martinarielhartmann@gmail.com>
2026-04-20 13:05:54 +0300

    mcrms: norm data support

--- src/mcrms.jl
+++ src/mcrms.jl
@@ -2,9 +2,16 @@ function mcrms(m::Union{Mocapdata,Normdata})
     r = nanrms(Matrix(m.data))
     return DataFrame(r,names(m.data))
 end
-function mcrms(m::Mocapdata,dim)
+function mcrms(m::Mocapdata,dim::Int64)
     @views r = nanrms(Matrix(m.data[:,dim:3:end]))
     return DataFrame(r,m.markerName)
+end
+function mcrms(m::Union{Mocapdata,Normdata}...)
+    ms = map(x -> Matrix(x.data),m)
+    nom = vec(nanrms(sum(cat(ms...;dims=3),dims=3)))
+    denom = vec(sum(nanrms.(ms)))
+    r = nom./denom
+    return DataFrame(r',names(m[1].data))
 end
 function nanrms(x)
     sumsq = sum(x -> isnan(x) ? 0.0 : x*x, x; dims=1)
