f283bbc0df19375ff0c130bbedd7deb1777968aa
Martin Hartmann <martinarielhartmann@gmail.com>
2026-04-20 13:06:14 +0300

    fast interpolation

--- src/mcresample.jl
+++ src/mcresample.jl
@@ -1,14 +1,17 @@
-function mcresample(m::Union{Mocapdata,Normdata}, newfreq::Int64)
-d1 = m.data
-    t1 = collect(0:(size(d1,1)-1))/m.freq
+function mcresample(m::Union{Mocapdata,Normdata}, newfreq::Int,new::String)
+    d1 = Matrix(m.data)
+    n = size(d1,1)
+    t1 = (0:n-1)./ m.freq
     t2 = 0:(1/newfreq):t1[end]
-    ser = FastInterpolations.Series(Matrix(d1)) # interpolate so as to get the unwrapped phase difference at each beat
+    ser = FastInterpolations.Series(d1) # interpolate so as to get the unwrapped phase difference at each beat
     itp = cubic_interp(t1,ser)
-    interpmat = hcat(itp(t2)...)
-    res = deepcopy(m)
-    res.data = DataFrame(interpmat,names(m.data))
-    res.freq = newfreq
-    res.times = DataFrame(Frame=1:size(t2,1), Time=collect(t2))
-    res.nFrames = size(res.data,1)
+    interpmat = Matrix{eltype(d1)}(undef, length(t2), size(d1, 2))
+    Threads.@threads for i in eachindex(t2)
+        @inbounds interpmat[i, :] = itp(t2[i])
+    end
+    res = @set m.data = DataFrame(interpmat,names(m.data))
+    res = @set m.freq = newfreq
+    res = @set m.times = DataFrame(Frame=1:size(t2,1), Time=collect(t2))
+    res = @set m.nFrames = size(interpmat,1)
     return res
 end
