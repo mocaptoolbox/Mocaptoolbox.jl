19f30c19ae60789604ddf0aa56f60ef7ffc0605f
Martin Hartmann <martinarielhartmann@gmail.com>
2026-04-27 17:00:13 +0300

    create norm data

--- src/mcinitstruct.jl
+++ src/mcinitstruct.jl
@@ -3,8 +3,7 @@ function mcinitstruct(df::DataFrame,fname::String,freq::Int,timederOrder::Int)
     df = df[:,2:end]
     nframes = size(df,1)
     nmarkers = size(df,2)
-    conn = zeros(Int,nmarkers,2)
     bp = string.(1:length(nmarkers))
     meta = ["no","metadata"]
-    mt = Mocapdata("Norm data",fname,nframes,nmarkers,freq,bp,df,meta,times,timederOrder,conn)
+    mt = Normdata("Norm data",fname,nframes,nmarkers,freq,bp,df,meta,times,timederOrder)
 end
